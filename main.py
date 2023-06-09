322612a7aef2c25ec2a398ead55dacf879ff026f
Angelly234 <100893298+Angelly234@users.noreply.github.com>
2023-06-09 12:05:25 +0700

    Co-authored-by: Kanold0 <Kanold0@users.noreply.github.com> Co-authored-by: Vongsorya <Vongsorya@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pygame, sys
-from setting import *
+from settings import *
+from level import Level
 
 class Game: 
-    def _init_(self)
+    def __init__(self): 
         pygame.init()
-        self.screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         pygame.display.set_caption('Sprout land')
-        self.clock = pygame.time.clock()
+        self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
+    
+if __name__ == '__main__':
+    game = Game()
+    game.run()
