afed798cbcb580952cc32a4df50e5d6072f8363e
Angelly234 <100893298+Angelly234@users.noreply.github.com>
2023-06-09 13:41:00 +0700

    Co-authored-by: Kanold0 <Kanold0@users.noreply.github.com> Co-authored-by: Vongsorya <Vongsorya@users.noreply.github.com>

--- timer.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Timer:
-    def __init__(self,duration,func =None):
-        self.duration=duration
-        self.func=func
-        self.start_time=0
-        self.active= False
-    
